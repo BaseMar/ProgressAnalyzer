72214a2249edee48fe0165008c751447fa18de50
BaseMar <msmsebastiani@gmail.com>
2025-07-07 12:24:43 +0200

    Dodano wykres progresu liniowy + tempo rozwoju

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 # Konfiguracja aplikacji
 st.set_page_config(page_title="Gym Progress Tracker", layout="wide")
@@ -43,14 +44,42 @@ def pokaz_trening():
     st.subheader("Twoje treningi")
 
     df = st.session_state["workouts"]
-
     if df.empty:
         st.info("Brak zapisanych treningów")
     else:
         st.dataframe(df, use_container_width=True)
 
+def pokaz_wykres():
+    st.subheader("Progress ciężaru")
+
+    df = st.session_state["workouts"]
+    if df.empty:
+        st.info("Brak danych do wyświetlenia")
+        return
+    
+    lista_ćwiczeń = df["Ćwiczenie"].unique().tolist()
+    if not lista_ćwiczeń:
+        st.info("Brak zapisanych ćwiczeń")
+        return
+    
+    wybrane_ćwiczenie = st.selectbox("Wybierz ćwiczenie", lista_ćwiczeń)
+    df_przefiltrowane = df[df["Ćwiczenie"] == wybrane_ćwiczenie].copy()
+    df_przefiltrowane["Data"] = pd.to_datetime(df_przefiltrowane["Data"])
+    df_zgrupowane = df_przefiltrowane.groupby("Data")["Ciężar"].max().reset_index()
+    
+    df_zgrupowane["Data"] = df_zgrupowane["Data"].dt.strftime('%Y-%m-%d')
+    df_zgrupowane["Progres %"] = df_zgrupowane["Ciężar"].pct_change()*100
+    df_zgrupowane["Progres %"] = df_zgrupowane["Progres %"].round(1).fillna(0.0)
+
+    fig = px.line(df_zgrupowane, x="Data", y="Ciężar", markers = True, title=f"Maksymalny ciężar - {wybrane_ćwiczenie}", labels={"Data": "Data", "Ciężar": "Ciężar (kg)"}, text="Ciężar")
+    fig.update_layout(xaxis_type='category')
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.subheader("Tempo rozwoju (% zmiany)")
+    st.dataframe(df_zgrupowane[["Data", "Ciężar", "Progres %"]], use_container_width=True)
+
 def main():
-    menu = st.sidebar.selectbox("Menu", ["Dodaj trening", "Zobacz trening"])
+    menu = st.sidebar.selectbox("Menu", ["Dodaj trening", "Zobacz trening", "Wykres progresu"])
 
     match menu:
         case "Dodaj trening":
@@ -58,6 +87,9 @@ def main():
         
         case "Zobacz trening":
             pokaz_trening()
+        
+        case "Wykres progresu":
+            pokaz_wykres()
 
 
 if __name__ == "__main__":
