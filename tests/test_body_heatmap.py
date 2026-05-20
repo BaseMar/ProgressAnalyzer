ffea1777c378c07b5af515a5bee24b7844936db6
Martino Sebastiani <msmsebastiani@gmail.com>
2026-05-20 09:14:09 +0200

    fix: coloring missing area of human body, fix colors in forearms

--- tests/test_body_heatmap.py
+++ tests/test_body_heatmap.py
@@ -1,4 +1,10 @@
-from ui.utils.body_heatmap import _excluded_overlay_mask
+from ui.utils.body_heatmap import (
+    BODY_IMAGE_PATH,
+    MUSCLE_GROUP_SEEDS,
+    _body_fill_mask,
+    _excluded_overlay_mask,
+    _mask_from_seeds,
+)
 
 
 def test_excluded_overlay_mask_covers_hands_and_feet():
@@ -23,3 +29,35 @@ def test_excluded_overlay_mask_covers_hands_and_feet():
 
     assert all(mask[y, x] for x, y in excluded_points)
     assert not any(mask[y, x] for x, y in included_points)
+
+
+def test_forearm_inner_panels_are_colored_as_forearms_not_obliques():
+    fill_mask = _body_fill_mask(BODY_IMAGE_PATH.stat().st_mtime_ns)
+    forearms_mask = _mask_from_seeds(fill_mask, MUSCLE_GROUP_SEEDS["Forearms"])
+    obliques_mask = _mask_from_seeds(fill_mask, MUSCLE_GROUP_SEEDS["Obliques"])
+
+    inner_forearm_points = [
+        (303, 434),
+        (593, 434),
+    ]
+
+    assert all(forearms_mask[y, x] for x, y in inner_forearm_points)
+    assert not any(obliques_mask[y, x] for x, y in inner_forearm_points)
+
+
+def test_heatmap_seeds_cover_front_thigh_and_upper_oblique_panels():
+    fill_mask = _body_fill_mask(BODY_IMAGE_PATH.stat().st_mtime_ns)
+    legs_mask = _mask_from_seeds(fill_mask, MUSCLE_GROUP_SEEDS["Legs"])
+    obliques_mask = _mask_from_seeds(fill_mask, MUSCLE_GROUP_SEEDS["Obliques"])
+
+    front_thigh_points = [
+        (363, 680),
+        (533, 680),
+    ]
+    upper_oblique_points = [
+        (390, 335),
+        (510, 335),
+    ]
+
+    assert all(legs_mask[y, x] for x, y in front_thigh_points)
+    assert all(obliques_mask[y, x] for x, y in upper_oblique_points)
